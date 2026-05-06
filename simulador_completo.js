b633b239710728965bee22f4017b833ee955f953
xfoliux <cristiangualan91@gmail.com>
2026-05-05 23:04:16 -0500

    se agrega funciones en los archivos

--- simulador_completo.js
+++ simulador_completo.js
@@ -31,11 +31,45 @@ function mostrarSeccion(id) {
 function guardarTasa() {
   let tasa = recuperarFloat("tasaInteres");
   if (tasa >= 10 && tasa <= 20) {
+    tasaInteres = tasa;
     mostrarTexto("mensajeTasa", "Tasa configurada correctamente: " + tasa + " %"); //mostrar texto: util.
   } else {
-    mostrarTexto("mensajeTasa", "La tasa debe estar entre 10 y 20");
+    mostrarTexto("mensajeTasa", "La tasa debe estar entre 10% y 20%");
   }
+}
 
+function guardarClientes() {
+  let cedula = document.getElementById("cedula").value;
+  let nombre = document.getElementById("nombre").value;
+  let apellido = document.getElementById("apellido").value;
+  let ingresos = document.getElementById("ingresos").value;
+  let egresos = document.getElementById("egresos").value;
+
+  let cliente = {
+    cedula : cedula,
+    nombre : nombre,
+    apellido : apellido,
+    ingresos : ingresos,
+    egresos : egresos
+  }
+  clientes.push(cliente);
+  pintarClientes();
+}
+
+function pintarClientes(){
+  let contenido = "";
+
+  for(i = 0 ; i < clientes.length ; i++){
+    contenido += "<tr>";
+    contenido += "<td>" + clientes[i].cedula + "</td>";
+    contenido += "<td>" + clientes[i].nombre + "</td>";
+    contenido += "<td>" + clientes[i].apellido + "</td>";
+    contenido += "<td>" + clientes[i].ingresos + "</td>";
+    contenido += "<td>" + clientes[i].egresos + "</td>";
+    contenido += "<td><button>Eliminar</button></td>";
+    contenido += "</tr>";
+  }
+  document.getElementById("tablaClientes").innerHTML = contenido;
 }
 
 
