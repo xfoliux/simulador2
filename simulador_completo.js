8bb112ec7abad0352b9a3527ffd0ff802255fb5e
xfoliux <cristiangualan91@gmail.com>
2026-05-06 00:12:01 -0500

    se agrega funciones de seleccionar cliente y limpiar formulario

--- simulador_completo.js
+++ simulador_completo.js
@@ -28,7 +28,7 @@ function mostrarSeccion(id) {
 }
 
 
-function guardarTasa() {
+function guardarTasa(){
   let tasa = recuperarFloat("tasaInteres");
   if (tasa >= 10 && tasa <= 20) {
     tasaInteres = tasa;
@@ -38,38 +38,115 @@ function guardarTasa() {
   }
 }
 
-function guardarClientes() {
-  let cedula = document.getElementById("cedula").value;
-  let nombre = document.getElementById("nombre").value;
-  let apellido = document.getElementById("apellido").value;
-  let ingresos = document.getElementById("ingresos").value;
-  let egresos = document.getElementById("egresos").value;
+function guardarCliente() {
+  let cedula = recuperaraTexto("cedula");
+  let nombre = recuperaraTexto("nombre");
+  let apellido = recuperaraTexto("apellido");
+  let ingresos = recuperarFloat("ingresos");
+  let egresos = recuperarFloat("egresos");
 
-  let cliente = {
-    cedula : cedula,
-    nombre : nombre,
-    apellido : apellido,
-    ingresos : ingresos,
-    egresos : egresos
+  // VALIDACIÓN
+  if (cedula == "" || nombre == "" || apellido == "" || isNaN(ingresos) || isNaN(egresos)) {
+    alert("Complete todos los campos correctamente");
+    return;
   }
-  clientes.push(cliente);
+
+  if (clienteSeleccionado == null) {
+    // INGRESO DE DATOS PERSONALES
+    let cliente = {
+      cedula,
+      nombre,
+      apellido,
+      ingresos,
+      egresos
+    };
+    clientes.push(cliente);
+
+  } else {
+    // ACTUALIZACION DE DATOS
+    clienteSeleccionado.nombre = nombre;
+    clienteSeleccionado.apellido = apellido;
+    clienteSeleccionado.ingresos = ingresos;
+    clienteSeleccionado.egresos = egresos;
+
+    clienteSeleccionado = null;
+  }
+
   pintarClientes();
+  limpiarFormulario();
 }
 
-function pintarClientes(){
+function pintarClientes() {
   let contenido = "";
 
-  for(i = 0 ; i < clientes.length ; i++){
+  for (let i = 0; i < clientes.length; i++) {
     contenido += "<tr>";
     contenido += "<td>" + clientes[i].cedula + "</td>";
     contenido += "<td>" + clientes[i].nombre + "</td>";
     contenido += "<td>" + clientes[i].apellido + "</td>";
     contenido += "<td>" + clientes[i].ingresos + "</td>";
     contenido += "<td>" + clientes[i].egresos + "</td>";
-    contenido += "<td><button>Eliminar</button></td>";
+
+    contenido += "<td>";
+    contenido += "<button onclick=\"seleccionarCliente('" + clientes[i].cedula + "')\">Actualizar</button>";
+    contenido += "<button onclick=\"eliminarCliente('" + clientes[i].cedula + "')\">Eliminar</button>";
+    contenido += "</td>";
+
     contenido += "</tr>";
   }
+
   document.getElementById("tablaClientes").innerHTML = contenido;
+}
+
+function eliminarCliente(cedula) {
+  for (let i = 0; i < clientes.length; i++) {
+    if (clientes[i].cedula === cedula) {
+      clientes.splice(i, 1);
+      break;
+    }
+  }
+
+  if (clienteSeleccionado != null && clienteSeleccionado.cedula === cedula) {
+    limpiarFormulario();
+  }
+  pintarClientes();
+}
+
+function seleccionarCliente(cedula) {
+  let cliente = buscarCliente(cedula);
+
+  if (cliente != null) {
+    mostrarTextoEnCaja("cedula", cliente.cedula);
+    mostrarTextoEnCaja("nombre", cliente.nombre);
+    mostrarTextoEnCaja("apellido", cliente.apellido);
+    mostrarTextoEnCaja("ingresos", cliente.ingresos);
+    mostrarTextoEnCaja("egresos", cliente.egresos);
+
+    clienteSeleccionado = cliente;
+  } else {
+    alert("Cliente no encontrado");
+  }
+}
+
+
+
+function limpiarFormulario() {
+  mostrarTextoEnCaja("cedula", "");
+  mostrarTextoEnCaja("nombre", "");
+  mostrarTextoEnCaja("apellido", "");
+  mostrarTextoEnCaja("ingresos", "");
+  mostrarTextoEnCaja("egresos", "");
+
+  clienteSeleccionado = null;
+}
+
+function buscarCliente(cedula){
+  for(let i = 0 ; i < clientes.length; i++){
+    if(clientes[i].cedula === cedula){
+      return clientes[i];
+    }
+  }
+  return null;
 }
 
 
